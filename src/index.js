c4b664ed828ed39c51334577fb865903499edffb
kkiilas <kadri.kiilas@helsinki.fi>
2022-04-19 19:48:41 +0300

    updated src/index.js and dependencies in part6-2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
@@ -13,9 +13,8 @@ const store = configureStore({
   }
 })
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
